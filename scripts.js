3d2362e2b6a33dde7bcad2b0272e3907b00a820e
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-04-18 14:24:16 -0700

    Styling and animations

--- scripts.js
+++ scripts.js
@@ -1,15 +1,40 @@
-const aboutButton = document.querySelector('.abtBtn');
-const landingOverlay = document.querySelector('.landing-overlay');
-const aboutOverlay = document.querySelectorAll('.about-overlay, .info-section-container, .info-image-container');
+const aboutButton = document.querySelector(".abtBtn");
+const landingOverlay = document.querySelector(".landing-overlay");
+const aboutOverlay = document.querySelectorAll(
+  ".about-overlay, .info-section-container, .info-image-container, .info-img, .main-bio, .main-name, .email, .instagram, .phone-number"
+);
 
 aboutButton.onclick = function showAboutOverlay() {
-  landingOverlay.classList.add('hidden');
+  landingOverlay.classList.add("hidden");
 
-  aboutOverlay.forEach( element => {
-    element.classList.remove('hidden');
-    
-    if (element.classList.value === 'about-overlay') {
-      element.classList.add('maxHeight');
+  aboutOverlay.forEach((element) => {
+    element.classList.remove("hidden");
+
+    if (element.classList.value === "about-overlay") {
+      element.classList.add("maxHeight");
     }
-  })
-}
+
+    if (element.classList.value === "main-name") {
+      element.classList.add("animateMainName");
+    }
+
+    if (element.classList.value === "phone-number") {
+      element.classList.add("animateSlideIn1");
+    }
+
+    if (element.classList.value === "email") {
+      element.classList.add("animateSlideIn2");
+    }
+
+    if (element.classList.value === "instagram") {
+      element.classList.add("animateSlideIn3");
+    }
+
+    if (
+      element.classList.value === "main-bio" ||
+      element.classList.value === "info-img"
+    ) {
+      element.classList.add("animateFadeIn");
+    }
+  });
+};
